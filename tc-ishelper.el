da2af92e681b270d3cc1647e161319aefaa1908f
Toru Takimoto <tooro88@outlook.jp>
2025-11-17 22:17:54 +0900

    Reimplement wrapped search using isearch-regexp-function
    tc-is22.el implements wrapped search by modifying internal Isearch
    functions to convert a search string into a regexp.  Since Emacs 25,
    `isearch-regexp-function` has provided a cleaner way to perform such
    conversions.
    
    Use `isearch-regexp-function` to reimplement wrapped search, and
    enable this implementation when `tcode-use-isearch` is `'advice`.
    Together with the change in the previous commit, this completes the
    reimplementation of the features previously provided by tc-is22.el.

--- tc-ishelper.el
+++ tc-ishelper.el
@@ -81,6 +81,58 @@
 (defun tcode-isearch-make-string-for-wrapping (s) s)
 
 ;;;
+;;; wrapped-search
+;;;
+
+(defun tcode--wrapped-search-regexp (string &optional lax)
+  "STRING または、STRING の日本語文字の前に行折り返しなどの空白が挟ま
+れた文字列にマッチする正規表現を返す。"
+  (mapconcat (lambda (ch)
+	       (let ((s-ch (char-to-string ch)))
+		 (cond ((= (char-width ch) 2)
+			(concat tcode-isearch-ignore-regexp s-ch))
+		       (t
+			(regexp-quote s-ch)))))
+	     (string-to-list string)
+	     nil))
+
+(defun tcode--wrapped-search-init-state ()
+  "isearch 開始時、必要に応じて wrapped-search を有効にする。"
+  (when (and isearch-mode
+	     tcode-isearch-enable-wrapped-search
+	     (boundp 'isearch-regexp-function) ; この2行は emacs-24 での
+	     (fboundp 'isearch-toggle-wrap)    ; compiler warning 対策。
+	     (not isearch-regexp)            ; regexp search でないとき
+	     (null isearch-regexp-function)) ; word/symbol search でないとき
+    (isearch-toggle-wrap)))
+
+;;;
+;;; isearch-toggle-wrap の定義
+;;;
+
+;; emacs-24 では isearch-regexp-function が無いので、このファイルの実
+;; 装方法では wrapped-search を提供できない。
+(defconst tcode--has-wrapped-search (fboundp 'isearch-define-mode-toggle)
+  "wrapped-search を実装可能な emacs バージョンかどうか。")
+
+(unless tcode--has-wrapped-search
+  ;; emacs-24 用ダミー実装。
+  (defmacro isearch-define-mode-toggle (&rest args)))
+
+(require 'cl-macs)
+(defvar tcode--toggle-wrap-previous-binding
+  (lookup-key isearch-mode-map (kbd "M-s @"))
+  "M-s @ のデフォルトのバインディング。")
+;; isearch 内のコマンド isearch-toggle-wrap を作り、"M-s @" にバインドする。
+;;  - キーバインドは特に必要ないが、このマクロの仕様上スキップできない。
+;;    一時的にバインドしてすぐに元に戻す。
+;;  - ???: このマクロはトップレベルに置かないとエラー。emacs -qでこの
+;;    ファイルだけをロードした場合に発生。原因不明。
+(isearch-define-mode-toggle wrap "@" tcode--wrapped-search-regexp
+  "Turning on wrap search turns off regexp mode.")
+(define-key isearch-mode-map (kbd "M-s @") tcode--toggle-wrap-previous-binding)
+
+;;;
 ;;; 初期化
 ;;;
 
@@ -90,7 +142,9 @@
   "tc-ishelper.el のロード時に初期化を行なう。"
   (when (eq tcode-use-isearch 'advice)
     (advice-add 'isearch-printing-char :around #'tcode--isearch-printing-char))
-  (add-hook 'isearch-mode-hook #'tcode-isearch-init))
+  (add-hook 'isearch-mode-hook #'tcode-isearch-init)
+  (when tcode--has-wrapped-search
+    (add-hook 'isearch-mode-hook #'tcode--wrapped-search-init-state)))
 
 (tcode--ishelper-init)
 (provide 'tc-ishelper)
