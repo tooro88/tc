9ca918d14ec63b62e7b9439c1178185095419367
Toru Takimoto <tooro88@outlook.jp>
2025-12-12 22:25:51 +0900

    Reimplement Isearch extensions using advice
    The Isearch extensions provided by tc-is22.el override several
    internal Emacs functions.  This approach is error-prone and can miss
    new features introduced in recent Emacs versions.
    
    tc-is22.el provides two features:
     1. line-fold search, which will be reimplemented in the next commit, and
     2. user input hooking, which is reimplemented here.
    
    Replace the modification of `isearch-printing-char` with advice that
    achieves the same behavior -- invoking the T-Code input method when
    necessary -- so that the existing bushu/mazegaki conversion code in
    tc-iscommon.el continues to work unchanged.

--- tc-ishelper.el
+++ tc-ishelper.el
@@ -21,4 +21,76 @@
 
 ;;; Code:
 
+(require 'tc-iscommon)
+
+;;;
+;;; advice による isearch 拡張の実装
+;;;
+
+(declare-function tcode-function-p    "tc")
+(declare-function tcode-apply-filters "tc")
+(declare-function tcode-decode-chars  "tc")
+
+(defun tcode--isearch-printing-char (orig-fun &optional char count)
+  "isearch 中に文字キーが押されると呼ばれる。必要に応じて T-Code 実装を呼ぶ。"
+  (if (bound-and-true-p tcode-mode)
+      (tcode--input-method-for-isearch char count)
+    (funcall orig-fun char count)))
+
+;; tc-is22.el の isearch-printing-char の追加部分を抜き出したもの。
+(defun tcode--input-method-for-isearch (char count)
+  "isearch 中の日本語入力。`tcode-input-method'の簡易版。"
+  (let* ((decoded (tcode-decode-chars char))
+	 (action (car decoded))
+	 (prev (tcode-isearch-bushu)))
+    (cond ((null action)
+	   (ding))
+	  ((stringp action)
+	   (setq action
+		 (mapconcat 'char-to-string
+			    (tcode-apply-filters
+			     (string-to-list action))
+			    nil))
+	   (tcode-isearch-process-string action prev))
+	  ((char-or-string-p action)
+	   (tcode-isearch-process-string
+	    (char-to-string (car (tcode-apply-filters (list action))))
+	    prev))
+	  ((and (not (tcode-function-p action))
+		(consp action))
+	   (tcode-isearch-process-string
+	    (mapconcat 'char-to-string
+		       (tcode-apply-filters
+			(mapcar 'string-to-char
+				(delq nil action)))
+		       nil)
+	    prev))
+	  ((tcode-function-p action)
+	   (let ((func (assq action
+			     tcode-isearch-special-function-alist)))
+	     (if func
+		 (funcall (or (cdr func)
+			      action))
+	       (tcode-isearch-process-string
+		(mapconcat 'char-to-string (cdr decoded) nil)
+		prev))))
+	  (t
+	   (ding)))))
+
+;; この関数の役割は tcode--line-fold-search-regexp に移った。
+(defalias 'tcode-isearch-make-string-for-line-fold #'identity)
+
+;;;
+;;; 初期化
+;;;
+
+(defvar tcode-use-isearch)
+
+(defun tcode--ishelper-init ()
+  "tc-ishelper.el のロード時に初期化を行なう。"
+  (when (eq tcode-use-isearch 'advice)
+    (advice-add 'isearch-printing-char :around #'tcode--isearch-printing-char))
+  (add-hook 'isearch-mode-hook #'tcode-isearch-init))
+
+(tcode--ishelper-init)
 (provide 'tc-ishelper)
